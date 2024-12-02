df899345a916a6e8c5018fee21de6a3bf6e498bf
Aidan Kinnaird <144266355+Impuse007@users.noreply.github.com>
2024-12-02 10:22:42 -0400

    Added cam animation

--- Assets/Scripts/CameraAnimTrigger.cs
+++ Assets/Scripts/CameraAnimTrigger.cs
@@ -5,19 +5,24 @@ using UnityEngine.UI;
 
 public class CameraAnimTrigger : MonoBehaviour
 {
-    public Button yourButton;
-    public Animator cameraAnimator;
+    public Button yourButton;      // Reference to the button
+    public Animator cameraAnimator;  // Reference to the camera's Animator
 
-
-    // Start is called before the first frame update
     void Start()
     {
+        // Disable the Animator to prevent it from playing immediately
+        cameraAnimator.enabled = false;
+
+        // Add listener for button click
         yourButton.onClick.AddListener(TriggerCameraAnimation);
     }
 
     void TriggerCameraAnimation()
     {
+        // Enable the Animator just before triggering the animation
+        cameraAnimator.enabled = true;
+
+        // Set the trigger to start the animation
         cameraAnimator.SetTrigger("PlayCameraMovement");
     }
-
 }
